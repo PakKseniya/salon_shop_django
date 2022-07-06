b787d9105e0736c6ecfdae04599f32ece7556703
PakKseniya <kseniya_gorelova95@mail.ru>
2022-07-06 11:21:54 +1000

    update_model_order

--- salon_site1/orders/models.py
+++ salon_site1/orders/models.py
@@ -4,14 +4,13 @@ from salon_shop.models import Product
 
 
 class Order(models.Model):
-    # user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='orders', verbose_name='Заказы')
     first_name = models.CharField(max_length=150, verbose_name='Имя')
     last_name = models.CharField(max_length=150, verbose_name='Фамилия')
     email = models.EmailField()
     address = models.CharField(max_length=250, verbose_name='Адрес доставки')
     postal_code = models.CharField(max_length=20, verbose_name='Индекс')
     city = models.CharField(max_length=100, verbose_name='Город доставки')
-    created = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False, verbose_name='Оплачен?')
 
@@ -29,10 +28,10 @@ class Order(models.Model):
 
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, related_name='order_items', on_delete=models.PROTECT)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    quantity = models.PositiveIntegerField(default=1)
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE, verbose_name='Заказ №')
+    product = models.ForeignKey(Product, related_name='order_items', on_delete=models.PROTECT, verbose_name='ID продукта')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена на момент заказа')
+    quantity = models.PositiveIntegerField(default=1, verbose_name='Количество')
 
     def __str__(self):
         return '{}'.format(self.id)
